cef201344629aa45f3e8f561323cc59ccee82c6f
Geert Stappers <stappers@stappers.nl>
2016-10-18 22:49:50 +0200

    Import Upstream version 2.11

--- log.c
+++ log.c
@@ -8,7 +8,7 @@
  *
  *	The license which is distributed with this software in the file
  *	COPYRIGHT applies to this software. If your distribution is missing
- *	this file, you may request it from <pekkas@netcore.fi>.
+ *	this file, you may request it from <reubenhwk@gmail.com>.
  *
  */
 
@@ -16,57 +16,57 @@
 #include "includes.h"
 #include "radvd.h"
 
-static int	log_method = L_NONE;
-static char *log_ident;
-static char *log_file;
+__attribute__ ((format(printf, 2, 0)))
+static int vlog(int prio, char const *format, va_list ap);
+
+static int log_method = L_NONE;
+static char const *log_ident;
+static char const *log_file;
 static FILE *log_file_fd;
 static int log_facility;
 static int debug_level = 0;
 
-int
-log_open(int method, char *ident, char *log, int facility)
+int log_open(int method, char const *ident, char const *log, int facility)
 {
 	log_method = method;
 	log_ident = ident;
 
 	switch (log_method) {
-		case L_NONE:
-		case L_STDERR:
-			break;
-		case L_STDERR_SYSLOG:
-			/* fallthrough */
-		case L_SYSLOG:
-			if (facility == -1)
-				log_facility = LOG_DAEMON;
-			else
-				log_facility = facility;
+	case L_NONE:
+	case L_STDERR:
+		break;
+	case L_STDERR_SYSLOG:
+		/* fallthrough */
+	case L_SYSLOG:
+		if (facility == -1)
+			log_facility = LOG_DAEMON;
+		else
+			log_facility = facility;
 
-			openlog(log_ident, LOG_PID, log_facility);
-			break;
-		case L_LOGFILE:
-			if (!log)
-			{
-				fprintf(stderr, "%s: no logfile specified\n", log_ident);
-				return (-1);
-			}
-			log_file = log;
-			if ((log_file_fd = fopen(log_file, "a")) == NULL)
-			{
-				fprintf(stderr, "%s: can't open %s: %s\n", log_ident, log_file, strerror(errno));
-				return (-1);
-			}
-			break;
-		default:
-			fprintf(stderr, "%s: unknown logging method: %d\n", log_ident, log_method);
-			log_method = L_NONE;
-			return (-1);
+		openlog(log_ident, LOG_PID, log_facility);
+		break;
+	case L_LOGFILE:
+		if (!log) {
+			fprintf(stderr, "%s (%d): no logfile specified\n", log_ident, getpid());
+			return -1;
+		}
+		log_file = log;
+		if ((log_file_fd = fopen(log_file, "a")) == NULL) {
+			fprintf(stderr, "%s (%d): can't open %s: %s\n", log_ident, getpid(), log_file, strerror(errno));
+			return -1;
+		}
+		break;
+	default:
+		fprintf(stderr, "%s (%d): unknown logging method: %d\n", log_ident, getpid(), log_method);
+		log_method = L_NONE;
+		return -1;
 	}
 	return 0;
 }
 
 /* note: [dfv]log() is also called from root context */
-static int
-vlog(int prio, char *format, va_list ap)
+__attribute__ ((format(printf, 2, 0)))
+static int vlog(int prio, char const *format, va_list ap)
 {
 	char tstamp[64], buff[1024];
 	struct tm *tm;
@@ -75,54 +75,51 @@ vlog(int prio, char *format, va_list ap)
 	vsnprintf(buff, sizeof(buff), format, ap);
 
 	switch (log_method) {
-		case L_NONE:
+	case L_NONE:
+		break;
+	case L_SYSLOG:
+		syslog(prio, "%s", buff);
+		break;
+	case L_STDERR_SYSLOG:
+		syslog(prio, "%s", buff);
+		if (prio > LOG_ERR)	/* fall through for messages with high priority */
 			break;
-		case L_SYSLOG:
-			syslog(prio, "%s", buff);
-			break;
-		case L_STDERR_SYSLOG:
-			syslog(prio, "%s", buff);
-			if (prio > LOG_ERR) /* fall through for messages with high priority */
-				break;
-		case L_STDERR:
-			current = time(NULL);
-			tm = localtime(&current);
-			(void) strftime(tstamp, sizeof(tstamp), LOG_TIME_FORMAT, tm);
+	case L_STDERR:
+		current = time(NULL);
+		tm = localtime(&current);
+		(void)strftime(tstamp, sizeof(tstamp), LOG_TIME_FORMAT, tm);
 
-			fprintf(stderr, "[%s] %s: %s\n", tstamp, log_ident, buff);
-    			fflush(stderr);
-			break;
-		case L_LOGFILE:
-			current = time(NULL);
-			tm = localtime(&current);
-			(void) strftime(tstamp, sizeof(tstamp), LOG_TIME_FORMAT, tm);
+		fprintf(stderr, "[%s] %s (%d): %s\n", tstamp, log_ident, getpid(), buff);
+		fflush(stderr);
+		break;
+	case L_LOGFILE:
+		current = time(NULL);
+		tm = localtime(&current);
+		(void)strftime(tstamp, sizeof(tstamp), LOG_TIME_FORMAT, tm);
 
-			fprintf(log_file_fd, "[%s] %s: %s\n", tstamp, log_ident, buff);
-    			fflush(log_file_fd);
-			break;
-		default:
-			fprintf(stderr, "%s: unknown logging method: %d\n", log_ident, log_method);
-			log_method = L_NONE;
-			return (-1);
+		fprintf(log_file_fd, "[%s] %s (%d): %s\n", tstamp, log_ident, getpid(), buff);
+		fflush(log_file_fd);
+		break;
+	default:
+		fprintf(stderr, "%s (%d): unknown logging method: %d\n", log_ident, getpid(), log_method);
+		log_method = L_NONE;
+		return -1;
 	}
 	return 0;
 }
 
-void
-dlog(int prio, int level, char *format, ...)
+void dlog(int prio, int level, char const *format, ...)
 {
-	va_list ap;
-
 	if (debug_level < level)
 		return;
 
+	va_list ap;
 	va_start(ap, format);
 	vlog(prio, format, ap);
 	va_end(ap);
 }
 
-void
-flog(int prio, char *format, ...)
+void flog(int prio, char const *format, ...)
 {
 	va_list ap;
 
@@ -131,36 +128,33 @@ flog(int prio, char *format, ...)
 	va_end(ap);
 }
 
-int
-log_close(void)
+int log_close(void)
 {
 	switch (log_method) {
-		case L_NONE:
-		case L_STDERR:
-			break;
-		case L_STDERR_SYSLOG:
-		case L_SYSLOG:
-			closelog();
-			break;
-		case L_LOGFILE:
-			fclose(log_file_fd);
-			break;
-		default:
-			fprintf(stderr, "%s: unknown logging method: %d\n", log_ident, log_method);
-			log_method = L_NONE;
-			return (-1);
+	case L_NONE:
+	case L_STDERR:
+		break;
+	case L_STDERR_SYSLOG:
+	case L_SYSLOG:
+		closelog();
+		break;
+	case L_LOGFILE:
+		fclose(log_file_fd);
+		break;
+	default:
+		fprintf(stderr, "%s (%d): unknown logging method: %d\n", log_ident, getpid(), log_method);
+		log_method = L_NONE;
+		return -1;
 	}
 	return 0;
 }
 
-void
-set_debuglevel(int level)
+void set_debuglevel(int level)
 {
 	debug_level = level;
 }
 
-int
-get_debuglevel(void)
+int get_debuglevel(void)
 {
 	return debug_level;
 }
--- log.h
+++ log.h
@@ -8,9 +8,11 @@
  *
  *   The license which is distributed with this software in the file COPYRIGHT
  *   applies to this software. If your distribution is missing this file, you
- *   may request it from <pekkas@netcore.fi>.
+ *   may request it from <reubenhwk@gmail.com>.
  *
  */
+
+#pragma once
 
 #define	L_NONE		0
 #define L_SYSLOG	1
@@ -20,9 +22,9 @@
 
 #define LOG_TIME_FORMAT "%b %d %H:%M:%S"
 
-int log_open(int, char *, char*, int);
-void flog(int, char *, ...);
-void dlog(int, int, char *, ...);
+int log_open(int, char const *, char const *, int);
+void flog(int, char const *, ...) __attribute__ ((format(printf, 2, 3)));
+void dlog(int, int, char const *, ...) __attribute__ ((format(printf, 3, 4)));
 int log_close(void);
 int log_reopen(void);
 void set_debuglevel(int);
